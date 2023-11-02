25d10752389297ed61ca0e97101ccff7f974890d
NahuelPalladini <palladininahuel@gmail.com>
2023-11-02 11:09:36 -0300

    Formularios

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 app.get('/register', (req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')))
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
-
+app.get('/profile', (req,res) => res.sendFile(path.join(__dirname, 'views', 'profile.html')))
 
 
 app.listen(PORT, () => console.log(`Server Running in http://localhost:${PORT}`))
